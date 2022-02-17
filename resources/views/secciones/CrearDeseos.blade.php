6b8830eb4078386c36f9c2638f244d4884ff97d5
jordioni1180 <jordioni1705@gmail.com>
2022-02-17 02:58:36 +0100

    Vistas y modelos

---
+++ resources/views/secciones/CrearDeseos.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.masterpage')
+@section('Titulo','Crear Deseo')
+@section('estilos')
+<link rel="stylesheet" href="{{URL::asset('css/CrearDeseos.css') }}">
+@endsection
+@section('contenido')
+
+@endsection
