97c511e094fba595a42c14c9604cb2c1d633e533
jordioni1180 <jordioni1705@gmail.com>
2022-02-21 00:30:25 +0100

    Modal de editar perfil

--- resources/views/secciones/perfil.blade.php
+++ resources/views/secciones/perfil.blade.php
@@ -19,7 +19,30 @@
             </div>
          </div>
          <div id="content" class="content content-full-width" style="height:60%;width:100%;border:none;overflow:auto;">
-            
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Editar Perfil
+            </button>
+  
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Editar Perfil</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form action="" method="">
+                        <div class="modal-body">
+                        ...
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                        </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
             @foreach ($usuario->deseos as $deseo)
             <div class="deseos">
                 <div class="card">
